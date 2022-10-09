11dea11fcc0dbc353d6d744b056322392e57cbec
Stefannia Thaina <78321194+thainabrito@users.noreply.github.com>
2022-10-08 22:00:04 -0300

    Add files via upload

---
+++ Trybank.cs
@@ -0,0 +1,97 @@
+﻿namespace trybank;
+
+public class Trybank
+{
+    public bool Logged;
+    public int loggedUser;
+    
+    //0 -> Número da conta
+    //1 -> Agência
+    //2 -> Senha
+    //3 -> Saldo
+    public int[,] Bank;
+    public int registeredAccounts;
+    private int maxAccounts = 50;
+    public Trybank()
+    {
+        loggedUser = -99;
+        registeredAccounts = 0;
+        Logged = false;
+        Bank = new int[maxAccounts, 4];
+    }
+
+    public void RegisterAccount(int number, int agency, int pass)
+    {
+                for (var i = 0; i < registeredAccounts; i++)
+        {
+            if (Bank[i, 0] == number && Bank[i, 1] == agency)
+            {
+                throw new ArgumentException("A conta já está sendo usada!");
+            }
+        }
+
+        Bank[registeredAccounts, 0] = number;
+        Bank[registeredAccounts, 1] = agency;
+        Bank[registeredAccounts, 2] = pass;
+        Bank[registeredAccounts, 3] = 0;
+        registeredAccounts++;
+    }
+
+    public void Login(int number, int agency, int pass)
+    {
+        bool accountExist = false;
+        if (Logged) throw new AccessViolationException("Usuário já está logado");
+        for (var i = 0; i < registeredAccounts; i++)
+        {
+            if (Bank[i, 0] != number || Bank[i, 1] != agency) continue;
+            if (Bank[i, 2] != pass) throw new ArgumentException("Senha incorreta");
+            Logged = true;
+            loggedUser = i;
+            accountExist = true;
+        }
+
+        if (!accountExist)
+        {
+            throw new ArgumentException("Agência + Conta não encontrada");
+        }
+    }
+
+    public void Logout()
+    {
+        if (!Logged) throw new AccessViolationException("Usuário não está logado");
+        Logged = false;
+        loggedUser = -99;
+    }
+
+    public int CheckBalance()
+    {
+        if (!Logged) throw new AccessViolationException("Usuário não está logado");
+        return Bank[loggedUser, 3];
+    }
+
+    public void Transfer(int destinationNumber, int destinationAgency, int value)
+    {
+        if (!Logged) throw new AccessViolationException("Usuário não está logado");
+        if (Bank[loggedUser, 3] < value) throw new InvalidOperationException("Saldo insuficiente");
+
+        Bank[loggedUser, 3] -= value;
+        for (var i = 0; i < registeredAccounts; i++)
+        {
+            if (Bank[i, 0] != destinationNumber || Bank[i, 1] != destinationAgency) continue;
+            Bank[i, 3] += value;
+        }
+    }
+
+    public void Deposit(int value)
+    {
+        if (!Logged) throw new AccessViolationException("Usuário não está logado");
+        Bank[loggedUser, 3] += value;
+    }
+
+    public void Withdraw(int value)
+    {
+        if (!Logged) throw new AccessViolationException("Usuário não está logado");
+        if (Bank[loggedUser, 3] < value) throw new InvalidOperationException("Saldo insuficiente");
+        Bank[loggedUser, 3] -= value;
+    }
+}
